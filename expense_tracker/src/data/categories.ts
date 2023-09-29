45a232b9bad90122f88178eccea4a43b5e6a35d9
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-09-29 15:40:49 -0300

    atualizando Category e importando o types

--- expense_tracker/src/data/categories.ts
+++ expense_tracker/src/data/categories.ts
@@ -1,5 +1,8 @@
-export const categories = {
-    food: { title: 'Alimentação', color: 'blue', expense: 'true' },
-    party: { title: 'Rolê', color: 'pink', expense: 'true' },
-    rent: { title: 'Aluguel', color: 'brown', expense: 'true' },
+import { Category } from "../types/category";
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color: 'blue', expense: true },
+    party: { title: 'Rolê', color: 'pink', expense: true },
+    rent: { title: 'Aluguel', color: 'brown', expense: true },
+    salary: { title: 'Salario', color: 'green', expense: false },
 };
