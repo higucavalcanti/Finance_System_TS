6b748afb490c494e2fedb06069cf9fd1c3c2d585
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-09-29 15:19:29 -0300

    criação de Item, types e categories

---
+++ expense_tracker/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'AnaSorvetes', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'role', title: 'Good Vibes', value: 60.25 },
+    { date: new Date(2021, 9, 16), category: 'role', title: 'Prainhaaa', value: 100 },
+    { date: new Date(2021, 10, 20), category: 'rent', title: 'Aluguel Apt', value: 228 },
+];
---
+++ expense_tracker/src/types/items.ts
@@ -0,0 +1,5 @@
+export const Item = {
+    food: { title: 'Alimentação', color: 'blue', expense: 'true' },
+    party: { title: 'Rolê', color: 'pink', expense: 'true' },
+    rent: { title: 'Aluguel', color: 'brown', expense: 'true' },
+};
