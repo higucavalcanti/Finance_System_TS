c58c4426db08ef684a98d40e42097a731eecaa2d
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-03 14:15:48 -0300

    Iniciando styles para TableArea

---
+++ expense_tracker/src/components/TableArea/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+    width: 100%;
+    background-color: #FFF;
+    paddind: 20px;
+    box-shadow:
+`;
+
+export const TableHeadColumn = styled.th``;
