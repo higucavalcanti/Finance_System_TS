2ed331393ed6be2dc6ab6ae2782ee57b48561756
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-07 19:48:09 -0300

    ...

--- expense_tracker/src/data/categories.ts
+++ expense_tracker/src/data/categories.ts
@@ -2,7 +2,7 @@ import { Category } from "../types/category";
 
 export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true },
-    party: { title: 'Rolê', color: 'pink', expense: true },
+    party: { title: 'Rolê', color: 'pink', expense: false },
     rent: { title: 'Aluguel', color: 'brown', expense: true },
     salary: { title: 'Salario', color: 'green', expense: false },
 };
