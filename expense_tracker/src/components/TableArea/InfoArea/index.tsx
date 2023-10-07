00725641d553995342f48d3f66438c4ce2bc62ff
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-07 20:36:52 -0300

    Criando Area para MÊs e Ano e o Props

--- expense_tracker/src/components/TableArea/InfoArea/index.tsx
+++ expense_tracker/src/components/TableArea/InfoArea/index.tsx
@@ -1,9 +1,21 @@
+import { formatCurrentMonth } from '../../../helpers/dateFilter';
 import * as C from './styles'
 
-export const InfoArea = () => {
+type Props = {
+    currentMonth: string
+}
+
+export const InfoArea = ({ currentMonth }: Props) => {
     return (
         <C.Container>
-            
+            <C.MonthArea>
+                <C.MonthArrow>⬅️</C.MonthArrow>
+                <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
+                <C.MonthArrow>➡️</C.MonthArrow>
+            </C.MonthArea>
+            <C.ResumeArea>
+
+            </C.ResumeArea>
         </C.Container>
     );
 } 
