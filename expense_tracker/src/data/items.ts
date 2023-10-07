98346b843c124780a748d248f4cac4a0d6e8b255
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-07 19:33:31 -0300

    consertando erro de definição

--- expense_tracker/src/data/items.ts
+++ expense_tracker/src/data/items.ts
@@ -1,9 +1,9 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    { date: new Date(2023, 9, 15), category: 'food', title: 'AnaSorvetes', value: 32.12 },
-    { date: new Date(2023, 9, 15), category: 'role', title: 'Good Vibes', value: 60.25 },
-    { date: new Date(2023, 9, 16), category: 'role', title: 'Prainhaaa', value: 100 },
+    { date: new Date(2023, 9, 9), category: 'food', title: 'AnaSorvetes', value: 32.12 },
+    { date: new Date(2023, 9, 15), category: 'party', title: 'Good Vibes', value: 60.25 },
+    { date: new Date(2023, 9, 16), category: 'party', title: 'Prainhaaa', value: 100 },
     { date: new Date(2023, 10, 4), category: 'rent', title: 'Aluguel Apt', value: 228 },
     { date: new Date(2023, 11, 2), category: 'salary', title: 'Salario', value: 4000 },
 ];
