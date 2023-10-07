f88497338b55947f7688a6d96ce7ded530610fd5
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-07 13:22:17 -0300

    importando TableArea, criando type, props e etc para ela

--- expense_tracker/src/App.tsx
+++ expense_tracker/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
 
         {/* Área de inserção */}
 
-        <TableArea />
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
--- expense_tracker/src/data/items.ts
+++ expense_tracker/src/data/items.ts
@@ -1,9 +1,9 @@
 import { Item } from "../types/Item";
 
 export const items: Item[] = [
-    { date: new Date(2021, 9, 15), category: 'food', title: 'AnaSorvetes', value: 32.12 },
-    { date: new Date(2021, 9, 15), category: 'role', title: 'Good Vibes', value: 60.25 },
-    { date: new Date(2021, 9, 16), category: 'role', title: 'Prainhaaa', value: 100 },
-    { date: new Date(2021, 10, 20), category: 'rent', title: 'Aluguel Apt', value: 228 },
-    { date: new Date(2021, 11, 2), category: 'salary', title: 'Salario', value: 4000 },
+    { date: new Date(2023, 9, 15), category: 'food', title: 'AnaSorvetes', value: 32.12 },
+    { date: new Date(2023, 9, 15), category: 'role', title: 'Good Vibes', value: 60.25 },
+    { date: new Date(2023, 9, 16), category: 'role', title: 'Prainhaaa', value: 100 },
+    { date: new Date(2023, 10, 4), category: 'rent', title: 'Aluguel Apt', value: 228 },
+    { date: new Date(2023, 11, 2), category: 'salary', title: 'Salario', value: 4000 },
 ];
