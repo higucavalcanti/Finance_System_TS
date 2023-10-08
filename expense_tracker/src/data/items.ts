0921493290ab22e5785fdf4f4f2681b7cc0adb98
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-08 12:29:43 -0300

    Corrigindo para teste

--- expense_tracker/src/data/items.ts
+++ expense_tracker/src/data/items.ts
@@ -5,5 +5,5 @@ export const items: Item[] = [
     { date: new Date(2023, 9, 15), category: 'party', title: 'Good Vibes', value: 60.25 },
     { date: new Date(2023, 9, 16), category: 'party', title: 'Prainhaaa', value: 100 },
     { date: new Date(2023, 10, 4), category: 'rent', title: 'Aluguel Apt', value: 228 },
-    { date: new Date(2023, 11, 2), category: 'salary', title: 'Salario', value: 4000 },
+    { date: new Date(2023, 10, 2), category: 'salary', title: 'Salario', value: 4000 },
 ];
